d42956fe870ce6a6cd91246fa9fe9da140e3ffd7
Howling Techie <howling.techie@gmail.com>
2024-06-10 12:05:20 +0100

    Created basic group browsing page

--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -1,9 +1,35 @@
+import {useContext, useEffect, useState} from "react";
+import {UserContext} from "../contexts/UserContext.tsx";
+import GroupInterface from "../interfaces/GroupInterface.ts";
+import {getGroups} from "../services/API.ts";
+import {GroupPreview} from "../components/Groups/GroupPreview.tsx";
+
 export const Groups = () => {
+    const currentUserContext = useContext(UserContext);
+
+    const [groups, setGroups] = useState<GroupInterface[]>([]);
+
+    useEffect(() => {
+        if (currentUserContext && currentUserContext.accessToken) {
+            currentUserContext.checkTokenStatus();
+            getGroups(currentUserContext.accessToken)
+                .then(data => setGroups(data.groups))
+                .catch(error => console.error("Error fetching groups", error));
+        }
+    }, [currentUserContext]);
 
     return (
         <>
-            <div className="text-l font-bold text-gray-900">Page Under Construction</div>
-            <div className="text-m text-gray-900">Page for groups</div>
+            <h1 className="text-2xl font-bold">Groups</h1>
+            {groups.length === 0 &&
+                <div>Loading Groups</div>}
+            {groups &&
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+                    {groups.map((group) => (
+                        <GroupPreview key={group.id} group={group}/>
+                    ))}
+                </div>
+            }
         </>
     );
 };
