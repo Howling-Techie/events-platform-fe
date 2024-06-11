503e53161193731844120552d444d2743fe4365e
Howling Techie <howling.techie@gmail.com>
2024-06-11 18:26:21 +0100

    Added support for events to have prices to attend, with Stripe to enable users to make payments towards events.

--- src/interfaces/UserInterface.ts
+++ src/interfaces/UserInterface.ts
@@ -10,5 +10,10 @@ export default interface UserInterface {
 
 export interface GroupUserInterface {
     user: UserInterface;
-    user_access_level: number;
+    user_access: number;
+}
+
+export interface EventUserInterface {
+    user: UserInterface;
+    status: { status: number, paid: boolean, amount_paid: number }
 }
