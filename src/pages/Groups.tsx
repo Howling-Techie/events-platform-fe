b2fab7c0ce9edabdb66363e50ae415e6697436f2
Howling Techie <howling.techie@gmail.com>
2024-06-13 17:17:08 +0100

    If a user is not logged in, they will only be shown public groups, and cannot create a group.

--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -7,13 +7,15 @@ import {GroupPreview} from "../components/Groups/GroupPreview.tsx";
 export const Groups = () => {
     const currentUserContext = useContext(UserContext);
 
-    const [groups, setGroups] = useState<GroupInterface[]>([]);
+    const [groups, setGroups] = useState<GroupInterface[]>();
 
     useEffect(() => {
-        if (currentUserContext && currentUserContext.accessToken) {
+        if (currentUserContext) {
             currentUserContext.checkTokenStatus();
             getGroups(currentUserContext.accessToken)
-                .then(data => setGroups(data.groups))
+                .then(data => {
+                    setGroups(data.groups);
+                })
                 .catch(error => console.error("Error fetching groups", error));
         }
     }, [currentUserContext]);
@@ -22,19 +24,20 @@ export const Groups = () => {
         <>
             <header className="flex flex-row space-x-2 items-center">
                 <h1 className="text-2xl font-bold">Groups</h1>
-                <a
+                {currentUserContext && currentUserContext.user && <a
                     href="/groups/new"
                     className="m-2 p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
                     aria-label="Create a Group"
                 >
                     Create a Group
                 </a>
+                }
             </header>
             {!groups ? (
                 <div>Loading Groups</div>
             ) : (
                 <div className="space-y-1 divide-y divide-gray-300">
-                    <div>
+                    {currentUserContext && currentUserContext.user && <div>
                         <h2 className="text-lg font-semibold">Your Groups</h2>
                         <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
                             {groups.filter(g => g.user_access_level).map((group) => (
@@ -42,6 +45,7 @@ export const Groups = () => {
                             ))}
                         </section>
                     </div>
+                    }
                     <div>
                         <h2 className="text-lg font-semibold mt-2">Public Groups</h2>
                         <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
