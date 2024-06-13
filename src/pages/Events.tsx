6f2c1f292ab71a579c5e7dcbd1aab2a6d5f83d2e
Howling Techie <howling.techie@gmail.com>
2024-06-13 17:06:33 +0100

    If the user is not logged in, they will only be shown public events.

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -10,7 +10,7 @@ export const Events = () => {
     const [events, setEvents] = useState<EventInterface[]>([]);
 
     useEffect(() => {
-        if (currentUserContext && currentUserContext.accessToken) {
+        if (currentUserContext) {
             currentUserContext.checkTokenStatus();
             getEvents(currentUserContext.accessToken)
                 .then(data => setEvents(data.events))
@@ -22,22 +22,22 @@ export const Events = () => {
         <>
             <header className="flex flex-row space-x-2 items-center">
                 <h1 className="text-2xl font-bold">Events</h1>
-                <a
+                {currentUserContext && currentUserContext.user && <a
                     href="/events/new"
                     className="m-2 p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
                     aria-label="Create an Event"
                 >
                     Create an Event
-                </a>
+                </a>}
             </header>
-            {events.length === 0 && <div>Loading Events</div>}
-            {events && (
-                <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-                    {events.map((event) => (
-                        <EventPreview key={event.id} event={event}/>
-                    ))}
-                </section>
-            )}
+            {!events ? (<div>Loading Events</div>)
+                : (
+                    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+                        {events.map((event) => (
+                            <EventPreview key={event.id} event={event}/>
+                        ))}
+                    </section>
+                )}
         </>
     );
 };
